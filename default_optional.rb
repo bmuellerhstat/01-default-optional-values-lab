c3e4d3f88da3406b9c5080e7dc04be621ff464b4
daysip5272 <daysip5272@hstat.org>
2016-10-05 17:16:49 +0000

    finish lab

---
+++ default_optional.rb
@@ -0,0 +1,6 @@
+def student_school(student,school)
+ "#{student.capitalize} is a student at #{school.upcase}."
+end
+def student_interests(name,interests, *args)
+    "#{name} is interested in #{interests}."
+end    
