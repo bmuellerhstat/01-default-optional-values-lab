ecba817cd9c18e85c4490165621f6f7b5b02ccee
daysip5272 <daysip5272@hstat.org>
2016-10-05 17:33:01 +0000

    finish lab adter review

--- default_optional.rb
+++ default_optional.rb
@@ -1,6 +1,6 @@
-def student_school(student,school)
- "#{student.capitalize} is a student at #{school.upcase}."
+def student_school(student,school= "HSTAT")
+ "#{student.capitalize} is a student at #{school}."
 end
-def student_interests(name,interests, *args)
-    "#{name} is interested in #{interests}."
+def student_interests(name,interests1, interests2, interests3)
+    "#{name.capitalize} is interested in [\"#{interests1}\", \"#{interests2}\", \"#{interests3}\"]."
 end    
