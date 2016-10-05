b223216a9fd5cfaa5775d32b615666784477d4aa
enriquev8884 <enriquev8884@hstat.org>
2016-10-05 17:16:25 +0000

    finish lab

---
+++ default_optional.rb
@@ -0,0 +1,13 @@
+def student_school(name="Diane", school="HSTAT")
+    "#{name.capitalize} is a student at #{school}."
+end
+def student_interests(name, *interest)
+    "#{name.capitalize} is interested in #{interest}."
+end
+
+def student_interests_proper(name, interest_1, interest_2, interest_3)
+    "#{name.capitalize} is interested in #{interest_1}, #{interest_2}, and #{interest_3}."
+end
+
+
+
