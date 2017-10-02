59f4d1389cac909affaa5770d1e6e7e69f807818
Kelly Lai <zhiyinl5633@hstat.org>
2017-10-02 20:58:54 +0000

    resolve failures

---
+++ default_optional.rb
@@ -0,0 +1,11 @@
+def student_school(name,school)
+    "#{name.capitalize} is a student at #{school}."
+end
+
+def student_school(name,school="HSTAT")
+    "#{name.capitalize} is a student at #{school}."
+end
+
+def student_interests(name,*interest)
+    "#{name.capitalize} is interested in #{interest}."
+end
