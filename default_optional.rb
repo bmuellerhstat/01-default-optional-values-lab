64571d66c727619fd4672d24576b126fca516a99
shifaq2337 <shifaq2337@hstat.org>
2017-10-02 21:45:08 +0000

    lab complete

--- default_optional.rb
+++ default_optional.rb
@@ -1,3 +1,7 @@
+def student_school(student, school)
+    "#{student.capitalize} is a student at #{school}."
+end
+
 def student_school(student, school="HSTAT")
     "#{student.capitalize} is a student at #{school}."
 end 
