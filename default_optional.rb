c2b4c558f11201d9abeb2ca76473232a609cfac8
christy8059 <christyl8059@hstat.org>
2017-10-03 01:08:19 +0000

    fix method syntax error

---
+++ default_optional.rb
@@ -0,0 +1,7 @@
+def student_school(name, school="HSTAT")
+"#{name.capitalize} is a student at #{school}."
+end 
+
+def student_interests(name,*interests)
+    "#{name.capitalize} is interested in #{interests}."
+end 
