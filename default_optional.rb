c17fa56efc08653d30092684f07190f31f1e4b5f
Hector Marin <hectorm6066@hstat.org>
2018-10-01 02:53:52 +0000

    Better than nothing

---
+++ default_optional.rb
@@ -0,0 +1,9 @@
+def student_school(name,school)
+    "#{name.capitalize} is a student at #{school}."
+end
+def student_school(name2)
+    "#{name2.capitalize} is a student at HSTAT."
+end
+def student_interests(name3, *thing)
+    "#{name3.capitalize} is interested in #{thing}."
+end
