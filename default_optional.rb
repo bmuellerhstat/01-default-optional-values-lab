b105ca2fbbc54492fe067246ab2b00e77877d160
Eva Noertoft <evan2304@hstat.org>
2018-10-01 20:55:38 +0000

    add correct methods

--- default_optional.rb
+++ default_optional.rb
@@ -1,9 +1,7 @@
-def method (a,b,c,d=3)
-    a+b+c+d
+def student_school(name,school="HSTAT")
+    "#{name.capitalize} is a student at #{school}."
 end
-puts method(2,2,3)
 
-def method1 (a,b=10,*c,d)
-"#{a}#{b}#{c}#{d}"
+def student_interests(name,*interests)
+    "#{name.capitalize} is interested in #{interests}."
 end 
-puts method1(3,4,5,6,7)
