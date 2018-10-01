d8d36a833d97cfd408c88d525a3d2a8aaf23cd70
Toby lau <yatchol6214@hstat.org>
2018-10-01 05:38:17 +0000

    edit default optional.rb

---
+++ default_optional.rb
@@ -0,0 +1,8 @@
+def student_school(name,school="HSTAT")
+    return "#{name.capitalize} is a student at #{school}."
+end
+
+def student_interests(name,*things_like) 
+    return "#{name.capitalize} is interested in #{things_like}."
+end
+
